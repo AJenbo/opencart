a3b9b656046df7935229a027eb275fdf8dccccc6
Anders Jenbo <anders@jenbo.dk>
2024-01-04 21:34:36 +0100

    Other fixes

--- upload/admin/controller/marketplace/modification.php
+++ upload/admin/controller/marketplace/modification.php
@@ -386,7 +386,7 @@ class Modification extends \Opencart\System\Engine\Controller {
 											$add = $operation->getElementsByTagName('add')->item(0)->textContent;
 											$trim = $operation->getElementsByTagName('add')->item(0)->getAttribute('trim');
 											$position = $operation->getElementsByTagName('add')->item(0)->getAttribute('position');
-											$offset = $operation->getElementsByTagName('add')->item(0)->getAttribute('offset');
+											$offset = (int)$operation->getElementsByTagName('add')->item(0)->getAttribute('offset');
 
 											if ($offset == '') {
 												$offset = 0;
